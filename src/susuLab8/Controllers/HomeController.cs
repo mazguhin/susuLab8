9a8fc9e418aa69ececa3756288bf81e93398c472
Mazguhin <mazguhin.evgen@gmail.com>
2017-03-12 16:30:47 +0500

    Start

---
+++ src/susuLab8/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace susuLab8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Time()
+        {
+            return Content(DateTime.Now.ToString("dd MMMM yyyy"));
+        }
+    }
+}
---
+++ src/susuLab8/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    getDate();
+
+    setInterval(function () {
+        getDate();
+    }, 3000);
+
+    function getDate() {
+        var query = $.ajax("/Home/Time")
+        .done(function (r) {
+            $('#dateNow').html(r);
+        });
+    };
+});
